e3af8e7476230dc3234a89333b11554090ee85bc
sxml <59968675@qq.com>
2024-10-02 14:13:55 +0800

    Update filebrowser.lua

--- luasrc/controller/filebrowser.lua
+++ luasrc/controller/filebrowser.lua
@@ -8,20 +8,16 @@ local api = require "luci.model.cbi.filebrowser.api"
 function index()
     if not nixio.fs.access("/etc/config/filebrowser") then return end
 
-    entry({"admin", "services"}, firstchild(), "Services", 44).dependent = false
-    entry({"admin", "services", "filebrowser"}, cbi("filebrowser/settings"),
-          _("File Browser"), 2).dependent = true
+    -- 确保 FileBrowser 插件显示在 NAS 组中
+    entry({"admin", "nas", "filebrowser"}, cbi("filebrowser/settings"),
+          _("File Browser"), 60).dependent = true
 
-    entry({"admin", "services", "filebrowser", "check"}, call("action_check")).leaf =
-        true
-    entry({"admin", "services", "filebrowser", "download"}, call("action_download")).leaf =
-        true
-    entry({"admin", "services", "filebrowser", "status"}, call("act_status")).leaf =
-        true
-    entry({"admin", "services", "filebrowser", "get_log"}, call("get_log")).leaf =
-        true
-    entry({"admin", "services", "filebrowser", "clear_log"}, call("clear_log")).leaf =
-        true
+    -- 添加功能项
+    entry({"admin", "nas", "filebrowser", "check"}, call("action_check")).leaf = true
+    entry({"admin", "nas", "filebrowser", "download"}, call("action_download")).leaf = true
+    entry({"admin", "nas", "filebrowser", "status"}, call("act_status")).leaf = true
+    entry({"admin", "nas", "filebrowser", "get_log"}, call("get_log")).leaf = true
+    entry({"admin", "nas", "filebrowser", "clear_log"}, call("clear_log")).leaf = true
 end
 
 local function http_write_json(content)
@@ -56,4 +52,7 @@ end
 function get_log()
     luci.http.write(luci.sys.exec("[ -f '/var/log/filebrowser.log' ] && cat /var/log/filebrowser.log"))
 end
-function clear_log() luci.sys.call("echo '' > /var/log/filebrowser.log") end
+
+function clear_log() 
+    luci.sys.call("echo '' > /var/log/filebrowser.log") 
+end
